be631767f1bdf367c567f990c3c6e6fd4b19eeac
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-04 10:39:42 -0300

    Update BookShelve.cs

--- Ejercicio 1/src/Library/BookShelve.cs
+++ Ejercicio 1/src/Library/BookShelve.cs
@@ -1,6 +1,6 @@
 namespace Ucu.Poo.Expert
 {
-    public class ShelveBook() //ShelveBook debería ser otra clase
+    public class ShelveBook //ShelveBook debería ser otra clase
       {
           public static void SendBookToShelve(Book libro, string sector, string shelve){
               libro.LibrarySector = sector;
