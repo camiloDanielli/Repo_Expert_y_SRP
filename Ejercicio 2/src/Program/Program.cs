e7569d9291a307c8f60d5f9ffeaf0f7a1817c99a
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-04 23:43:43 -0300

    Update Program.cs

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
-            Console.WriteLine(appointmentResult);
+            persona1 = new Person("Steven Jhonson", "5555-555-555", "986782342");
+            doctor1 = new Doctor("Wall Street", "Armand");
+            persona2 = new Person("Ralf Manson", "5555-555-555", "");
+            doctor2 = new Doctor("", "Queen Street");
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            string appointment1 = AppointmentService.CreateAppointment(persona1, doctor1);
+            string appointment2 = AppointmentService.CreateAppointment(persona2, doctor2);
+
+            Console.WriteLine(appointmentResult);
             Console.WriteLine(appointmentResult2);
         }
     }
