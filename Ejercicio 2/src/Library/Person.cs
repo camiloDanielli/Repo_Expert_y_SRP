60e40208f2d30966fc770156503e715b53e58d26
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-04 23:30:42 -0300

    Update Person.cs

--- Ejercicio 2/src/Library/Person.cs
+++ Ejercicio 2/src/Library/Person.cs
@@ -2,6 +2,17 @@ namespace Library
 {
   public class Person
   {
-    
+    public string name{ get; }
+    public string id { get; }
+    public string phoneNumber { get; }
+
+    public List<string> Propierties()
+    {
+      List<string> propiedades = new List<string>();
+      propiedades.Add(name);
+      propiedades.Add(id);
+      propiedades.Add(phoneNumber);
+      return propiedades;
+    }    
   }
 }
