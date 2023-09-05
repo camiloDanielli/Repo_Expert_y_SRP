8ac766feb5ae31495753eaa3b7f81d62607a65f2
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-04 23:25:08 -0300

    Update Validator.cs

--- Ejercicio 2/src/Library/Validator.cs
+++ Ejercicio 2/src/Library/Validator.cs
@@ -8,19 +8,22 @@ namespace Library
       {
         if (string.IsNullOrEmpty(propiedad))
         {
-          
+          return $"Unable to create, {propiedad} is missing";
         }
       }
+      return "Person validated";
+    }
 
-      
-      if (string.IsNullOrEmpty(data))
+    public static string ValidateDoctor(Doctor doctor)
+    {
+      foreach propiedad in doctor.Propierties()
       {
-        return = false;
+        if (string.IsNullOrEmpty(propiedad))
+        {
+          return $"Unable to create, {propiedad} is missing";
+        }
       }
-      else
-      {
-        return = true;
-      }
+      return "Doctor validated";
     }
   }
 }
