73f1ee6e86bea5d1f327bfc88412adfde25e8bb5
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-05 00:19:35 -0300

    Update Doctor.cs

--- Ejercicio 2/src/Library/Doctor.cs
+++ Ejercicio 2/src/Library/Doctor.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Library
 {
   public class Doctor
@@ -13,10 +15,20 @@ namespace Library
 
     public List<string> Propierties()
     {
+      
       List<string> propiedades = new List<string>();
       propiedades.Add(doctorName);
       propiedades.Add(appointmentPlace);
       return propiedades;
     }    
+
+    public List<string> PropiertiesNames()
+    {
+      List<string> listaPropiedades = new List<string>();
+      listaPropiedades.Add("name");
+      listaPropiedades.Add("appointment place");
+      return listaPropiedades;
+    }
+      
   }
 }
