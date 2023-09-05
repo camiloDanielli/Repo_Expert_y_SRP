7d392f0bbd0da2817d400d7adf79a543a0ed83b0
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-04 23:21:43 -0300

    Update Validator.cs

--- Ejercicio 2/src/Library/Validator.cs
+++ Ejercicio 2/src/Library/Validator.cs
@@ -2,8 +2,17 @@ namespace Library
 {
   public class Validator
   {
-    public static string Validate(string data)
+    public static string ValidatePerson(Person persona)
     {
+      foreach propiedad in persona.Propierties()
+      {
+        if (string.IsNullOrEmpty(propiedad))
+        {
+          
+        }
+      }
+
+      
       if (string.IsNullOrEmpty(data))
       {
         return = false;
