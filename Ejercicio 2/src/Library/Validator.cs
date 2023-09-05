98c0f980aab791159dac125954ed07083da9c21c
Leandro-Barral <141652604+Leandro-Barral@users.noreply.github.com>
2023-09-05 00:21:25 -0300

    Update Validator.cs

--- Ejercicio 2/src/Library/Validator.cs
+++ Ejercicio 2/src/Library/Validator.cs
@@ -4,24 +4,28 @@ namespace Library
   {
     public static string ValidatePerson(Person persona)
     {
-      foreach propiedad in persona.Propierties()
+      int counter = 0;
+      foreach (string propiedad in persona.Propierties())
       {
         if (string.IsNullOrEmpty(propiedad))
         {
-          return $"Unable to create, {propiedad} is missing";
+          return $"Unable to create, the person´s {persona.PropiertiesNames()[counter]} is missing";
         }
+        counter++;
       }
       return "Person validated";
     }
 
     public static string ValidateDoctor(Doctor doctor)
     {
-      foreach propiedad in doctor.Propierties()
+      int counter = 0;
+      foreach (string propiedad in doctor.Propierties())
       {
         if (string.IsNullOrEmpty(propiedad))
         {
-          return $"Unable to create, {propiedad} is missing";
+          return $"Unable to create, the doctor´s {doctor.PropiertiesNames()[counter]} is missing";
         }
+        counter++;
       }
       return "Doctor validated";
     }
